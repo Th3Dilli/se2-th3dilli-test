d9cee1ef5f1a5c0225c2de94f8fd52cdfcfaeefa
th3dilli <th3dilli@gmx.at>
2020-04-08 13:01:22 +0200

    refactored build.gradle's add common module trying to figure out how the heck to build a jar with the commmn module inside the server jar

---
+++ common/src/main/java/com/mygdx/Protocol/Protocol.java
@@ -0,0 +1,7 @@
+package com.mygdx.Protocol;
+
+public class Protocol {
+    static public void kek(){
+        System.out.println("reee");
+    }
+}
---
+++ server/src/main/java/com/mygdx/Server/Server.java
@@ -0,0 +1,12 @@
+package com.mygdx.Server;
+
+
+import com.mygdx.Protocol.Protocol;
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("hi from server");
+        Protocol.kek();
+    }
+}
